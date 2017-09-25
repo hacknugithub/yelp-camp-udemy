cd4162da092646be040a37352331df20ea623a02
Manuel Guerrero <manu_pacu@hotmail.com>
2017-09-25 03:44:34 +0000

    Styling with bootstrap and adding custom css

---
+++ v5/app.js
@@ -0,0 +1,104 @@
+
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    seedDB     = require("./seeds");
+
+
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+seedDB();
+
+app.get("/", function(req, res){
+    res.render("landing"); 
+});
+
+//INDEX ROUTE
+app.get("/campgrounds", function(req, res){
+    //GET ALL CAMPGROUNDS FROM DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            //console.log(campgrounds);
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/campgrounds/new", function(req, res) {
+   res.render("campgrounds/new"); 
+});
+//CREATE ROUTE
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name:name, image:image, description: desc};
+   //Create a new campground and save to DB
+   Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else{
+           res.redirect("/campgrounds");
+       }
+   });
+});
+//SHOW ROUTE
+app.get("/campgrounds/:id", function(req, res) {
+    
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+//=====================================================================
+//            COMENTS ROUTES
+//=====================================================================
+
+//NEW FOR COMMENTS
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("comments/new", {campground:campground});
+        }
+    });
+});
+
+//CREATE FOR COMMENTS
+app.post("/campgrounds/:id/comments", function(req, res){
+    
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err)
+                } else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+campground._id);
+                }
+            });
+        }
+    })
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Yelp Camp Server Started");
+});
